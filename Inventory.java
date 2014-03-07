dfe17405e26462b3d0d94381875357b98ac72f23
kazzacarrot124 <k.a.z.mcc95@gmail.com>
2014-03-07 20:58:41 +0000

    Create Inventory.java
    please fill with code and amend!!

---
+++ Inventory.java
@@ -0,0 +1,13 @@
+package missionSpoon;
+
+
+
+public class Inventory {
+  public String[] allObjects = new ArrayList;
+  
+  public void addObject (String arg) {
+    all objects.add (arg);
+  }
+
+
+}
